cb21314b2df368d8e122ebf508b77359940e4a39
Khoa Nguyen <64112482+19127445@users.noreply.github.com>
2024-04-16 13:20:36 +0700

    First build UI for the Header

--- website/src/components/HeaderComponent/HeaderComponent.jsx
+++ website/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
-
+import { Col } from "antd";
+import React from "react";
+import {
+  WrapperAccountHeader,
+  WrapperHeader,
+  WrapperTextHeader,
+  WrapperTextHeaderSmall,
+} from "./style";
+import Search from "antd/es/transfer/search";
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined,
+} from "@ant-design/icons";
 const HeaderComponent = () => {
   return (
-    <div>HeaderComponent</div>
-  )
-}
+    <div>
+      <WrapperHeader gutter={16}>
+        <Col span={6}>
+          <WrapperTextHeader>AKHOA-MTHU</WrapperTextHeader>
+        </Col>
+        <Col span={12}>
+          <Search
+            placeholder="input search text"
+            // allowClear
+            enterButton="Search"
+            size="large"
+            // onSearch={onSearch}
+          />
+        </Col>
+        <Col span={6} style={{display:'flex',gap:'20px'}}>
+          <WrapperAccountHeader>
+            <UserOutlined style={{ fontSize: "30px" }} />
+            <div>
+              <WrapperTextHeaderSmall>Đăng nhập/Đăng ký</WrapperTextHeaderSmall>
+              <div>
+                <span style={{ fontSize: "12px" }}>Tài khoản</span>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WrapperAccountHeader>
+          <div>
+            <ShoppingCartOutlined style={{ fontSize: "30px", color: "#fff" }} />
+            <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+          </div>
+        </Col>
+      </WrapperHeader>
+    </div>
+  );
+};
 
-export default HeaderComponent
+export default HeaderComponent;
