6d1c839213f96a69dedf158214408194f587fb57
Khoa Nguyen <khoanguyen1799x@gmail.com>
2024-04-17 10:24:33 +0700

    Finish the Image Slider

--- website/src/components/SliderComponent/SliderComponent.jsx
+++ website/src/components/SliderComponent/SliderComponent.jsx
@@ -1,8 +1,25 @@
+import { Image } from "antd";
+import Slider from "react-slick";
 
-const SliderComponent = ({arrImages}) => {
+const SliderComponent = ({ arrImages }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
   return (
-    <div>SliderComponent</div>
-  )
-}
+    <div>
+      <Slider {...settings}>
+        {arrImages.map((image) => {
+          return <Image src={image} alt="slider" preview={false} />;
+        })}
+      </Slider>
+    </div>
+  );
+};
 
-export default SliderComponent
+export default SliderComponent;
--- website/src/pages/HomePage/HomePage.jsx
+++ website/src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,30 @@
 import SliderComponent from "../../components/SliderComponent/SliderComponent";
 import TypeProduct from "../../components/TypeProduct/TypeProduct";
 import { WrapperTypeProduct } from "./style";
-import slider1 from '../../assets/images/slider1.webp'
-import slider2 from '../../assets/images/slider2.webp'
-import slider3 from '../../assets/images/slider3.webp'
+import slider1 from "../../assets/images/slider1.webp";
+import slider2 from "../../assets/images/slider2.webp";
+import slider3 from "../../assets/images/slider3.webp";
+import slider4 from "../../assets/images/slider4.webp";
 const HomePage = () => {
   const arr = ["TV", "Tủ lạnh", "Laptop"];
 
   return (
-    <div style={{ padding: "0 120px" }}>
-      <WrapperTypeProduct>
-        {arr.map((item) => {
-          return <TypeProduct name={item} key={item} />;
-        })}
-      </WrapperTypeProduct>
-      <SliderComponent arrImages={[slider1,slider2,slider3]}/>
-    </div>
+    <>
+      <div style={{ padding: "0 120px" }}>
+        <WrapperTypeProduct>
+          {arr.map((item) => {
+            return <TypeProduct name={item} key={item} />;
+          })}
+        </WrapperTypeProduct>
+      </div>
+
+      <div
+        id="container"
+        style={{ backgroundColor: "#efefef", padding: "0 120px" }}
+      >
+        <SliderComponent arrImages={[slider1, slider2, slider3,slider4]} />
+      </div>
+    </>
   );
 };
 
