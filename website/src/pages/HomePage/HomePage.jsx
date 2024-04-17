d0cb21dabc9a957ae7cf3272003b22a8ad3d46a6
Khoa Nguyen <64112482+19127445@users.noreply.github.com>
2024-04-17 19:29:20 +0700

    Create the Left Navbar

--- website/src/pages/HomePage/HomePage.jsx
+++ website/src/pages/HomePage/HomePage.jsx
@@ -6,6 +6,7 @@ import slider2 from "../../assets/images/slider2.webp";
 import slider3 from "../../assets/images/slider3.webp";
 import slider4 from "../../assets/images/slider4.webp";
 import CardComponent from "../../components/CardComponent/CardComponent";
+import NavbarComponent from "../../components/NavbarComponent/NavbarComponent";
 const HomePage = () => {
   const arr = ["TV", "Tủ lạnh", "Laptop"];
 
@@ -38,6 +39,7 @@ const HomePage = () => {
         >
           <CardComponent />
         </div>
+        <NavbarComponent/>
       </div>
     </>
   );
