1c2bd13074be21f9163e52a6bb032b2138843d12
Khoa Nguyen <khoanguyen1799x@gmail.com>
2024-04-22 10:46:23 +0700

    Create 3 models and connect to MongoDB successfully

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,17 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello world AKHOA-MTHU");
 });
+
+mongoose
+  .connect(
+    `mongodb+srv://khoa:${process.env.MONGO_DB_PASSWORD}@cluster0.uin9rsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(() => {
+    console.log("Connect DB success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log("Server is running on port", +port);
