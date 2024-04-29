b881cdae2669fe6b6ac4afa94baa2df394bf1302
Khoa Nguyen <khoanguyen1799x@gmail.com>
2024-04-29 11:06:48 +0700

    Build a User Registration API

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -3,8 +3,27 @@ const UserService = require("../services/UserService");
 const createUser = async (req, res) => {
   try {
     console.log(req.body);
-    const res = await UserService.createUser();
-    return res.status(200).json();
+    const { name, email, password, confirmPassword, phone } = req.body;
+    const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    const isCheckEmail = reg.test(email);
+    if (!name || !email || !password || !confirmPassword || !phone) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "The input is required",
+      });
+    } else if (!isCheckEmail) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "The input is email",
+      });
+    } else if (confirmPassword !== password) {
+      return res.status(200).json({
+        status: "ERROR",
+        message: "The Password is different from Confirm Password",
+      });
+    }
+    const respone = await UserService.createUser(req.body);
+    return res.status(200).json(respone);
   } catch (error) {
     return res.status(404).json({
       message: error,
